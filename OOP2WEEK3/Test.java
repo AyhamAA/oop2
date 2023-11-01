71c4891b970f4671d2f05ab92cf1e0c4ee4dda53
AyhamAA <149321612+AyhamAA@users.noreply.github.com>
2023-11-01 19:57:59 +0300

    Create Test.java

---
+++ OOP2WEEK3/Test.java
@@ -0,0 +1,22 @@
+package CirPKJ;
+public class Circle {
+    public static final double PI=3.14 ; 
+    private int rad ; 
+
+    public Circle() {
+    }
+
+    public Circle(int rad) {
+        this.rad = rad;
+    }
+    
+    public int getRad() {
+        return rad;
+    }
+
+    public void setRad(int rad) {
+        this.rad = rad;
+    }
+    
+    
+}
